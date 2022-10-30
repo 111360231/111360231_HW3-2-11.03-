509bd2a0601e1217bdd59d03d54856b709ab1660
111360231 <vivian14053@gmail.com>
2022-10-30 14:28:49 +0800

    finish

---
+++ 5.28/source/Main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char change(char A);
+
+int main(void)
+{
+	char S;
+	printf("輸入大寫英文字母，會回傳小字母：");
+	scanf_s("%c", &S);
+	
+	printf("小寫字母為:%c\n", change(S));
+
+	system("pause");
+	return 0;
+}
+
+char change(char A)
+{
+	int x; 
+	x = A;
+	x = x + 32;
+	A = x;
+	return A;
+}
